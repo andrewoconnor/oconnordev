a7e105cd30a1116ffe2d67fbb34c81e1806a45d2
Andrew O'Connor <andrewoconnor@outlook.com>
2024-05-17 20:14:46 -0400

    test terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50.0"
+    }
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.16.0.0/16"
+}
