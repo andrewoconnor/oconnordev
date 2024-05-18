145b578ca5527b81b0ff6c3312a2d07166306f2e
Andrew O'Connor <andrewoconnor@outlook.com>
2024-05-18 17:42:16 -0400

    test manage spacelift stacks

--- infra/stacks/main.tf
+++ infra/stacks/main.tf
@@ -60,7 +60,7 @@ resource "aws_iam_role" "spacelift" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      jsondecode(data.spacelift_aws_integration_attachment_external_id.my_stack.assume_role_policy_statement),
+      jsondecode(data.spacelift_aws_integration_attachment_external_id.general.assume_role_policy_statement),
     ]
   })
 }
