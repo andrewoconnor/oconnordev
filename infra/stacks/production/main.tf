53ddcead45361093d173e383103ab0f24ab6c2f8
Andrew O'Connor <andrewoconnor@outlook.com>
2024-06-19 12:59:09 -0400

    create cert

--- infra/stacks/production/main.tf
+++ infra/stacks/production/main.tf
@@ -186,3 +186,35 @@ resource "aws_s3_bucket_policy" "web_tls" {
 
   policy = data.aws_iam_policy_document.web_tls.json
 }
+
+resource "aws_acm_certificate" "oconnordev" {
+  domain_name       = local.zone_name
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${local.zone_name}"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "oconnordev_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.oconnordev.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+   # Skips the domain if it doesn't contain a wildcard
+    if length(regexall("\\*\\..+", dvo.domain_name)) > 0
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.oconnordev.id
+}
