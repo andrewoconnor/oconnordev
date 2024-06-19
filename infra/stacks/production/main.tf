b1db495340072df56473939c285f60dcd07aa474
Andrew O'Connor <andrewoconnor@outlook.com>
2024-06-19 13:57:24 -0400

    create cloudfront distribution

--- infra/stacks/production/main.tf
+++ infra/stacks/production/main.tf
@@ -24,6 +24,7 @@ data "aws_caller_identity" "current" {}
 locals {
   zone_name       = "oconnor.dev"
   web_bucket_name = "oconnordev-web"
+  s3_origin_id    = "oconnordevS3Origin"
 }
 
 resource "aws_route53_zone" "oconnordev" {
@@ -217,4 +218,43 @@ resource "aws_route53_record" "oconnordev_validation" {
   ttl             = 60
   type            = each.value.type
   zone_id         = aws_route53_zone.oconnordev.id
+}
+
+resource "aws_cloudfront_origin_access_control" "oconnordev" {
+  name                              = "oconnordev"
+  description                       = "oconnordev S3 policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "oconnordev" {
+  origin {
+    domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oconnordev.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 }
