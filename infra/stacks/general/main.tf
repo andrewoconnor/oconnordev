594badc0f57ab162dbff12d6c66b181ce98a4475
Andrew O'Connor <andrewoconnor@outlook.com>
2025-02-17 11:28:04 -0500

    output account_id

--- infra/stacks/general/main.tf
+++ infra/stacks/general/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.50.0"
+      version = "~> 5.87.0"
     }
   }
 }
@@ -11,7 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.16.0.0/16"
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
