e4d4e67eb5b2a5165d092ce218a00c7ef1287b9a
Andrew O'Connor <andrewoconnor@outlook.com>
2026-04-03 15:54:49 -0400

    temp no cloudfront

--- infra/stacks/drumrollworld/main.tf
+++ infra/stacks/drumrollworld/main.tf
@@ -166,65 +166,65 @@ resource "aws_route53_record" "drumrollworld_validation" {
   zone_id         = aws_route53_zone.drumrollworld.id
 }
 
-resource "aws_cloudfront_origin_access_control" "drumrollworld" {
-  name                              = "drumrollworld"
-  description                       = "drumrollworld S3 policy"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-resource "aws_cloudfront_distribution" "drumrollworld" {
-  origin {
-    domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.drumrollworld.id
-    origin_id                = local.s3_origin_id
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  aliases = [
-    local.zone_name,
-    "www.${local.zone_name}"
-  ]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.drumrollworld.arn
-    minimum_protocol_version = "TLSv1.2_2021"
-    ssl_support_method       = "sni-only"
-  }
-}
-
+#resource "aws_cloudfront_origin_access_control" "drumrollworld" {
+#  name                              = "drumrollworld"
+#  description                       = "drumrollworld S3 policy"
+#  origin_access_control_origin_type = "s3"
+#  signing_behavior                  = "always"
+#  signing_protocol                  = "sigv4"
+#}
+#
+#resource "aws_cloudfront_distribution" "drumrollworld" {
+#  origin {
+#    domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
+#    origin_access_control_id = aws_cloudfront_origin_access_control.drumrollworld.id
+#    origin_id                = local.s3_origin_id
+#  }
+#
+#  enabled             = true
+#  is_ipv6_enabled     = true
+#  default_root_object = "index.html"
+#
+#  aliases = [
+#    local.zone_name,
+#    "www.${local.zone_name}"
+#  ]
+#
+#  default_cache_behavior {
+#    allowed_methods  = ["GET", "HEAD"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = local.s3_origin_id
+#
+#    forwarded_values {
+#      query_string = false
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    viewer_protocol_policy = "redirect-to-https"
+#    min_ttl                = 0
+#    default_ttl            = 3600
+#    max_ttl                = 86400
+#  }
+#
+#  price_class = "PriceClass_100"
+#
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "whitelist"
+#      locations        = ["US", "CA", "GB", "DE"]
+#    }
+#  }
+#
+#  viewer_certificate {
+#    acm_certificate_arn      = aws_acm_certificate.drumrollworld.arn
+#    minimum_protocol_version = "TLSv1.2_2021"
+#    ssl_support_method       = "sni-only"
+#  }
+#}
+#
 #resource "aws_route53_record" "www" {
 #  zone_id = aws_route53_zone.drumrollworld.zone_id
 #  name    = "www.${local.zone_name}"
